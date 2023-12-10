07b2fff635b09c53ab0d0a778a8af686523477c7
Liger <gjfleo@yeah.net>
2023-12-11 00:50:42 +0800

    update

--- genshin-impact/ost/schema.ts
+++ genshin-impact/ost/schema.ts
@@ -1,29 +1,77 @@
 export interface GenshinImpactOSTData {
-    albums: GenshinImpactOSTAlbum[];
+  albums: GenshinImpactOSTAlbum[];
 }
 
 interface GenshinImpactOSTAlbumBase {
-    name: string;
-    nameEn: string;
-    releaseDate: string;
+  name: string;
+  nameEn: string;
+  releaseDate: string;
 }
 
 interface GenshinImpactOSTAlbumWithoutDiscs extends GenshinImpactOSTAlbumBase {
-    songs: GenshinImpactOSTSong[];
+  songs: GenshinImpactOSTSong[];
 }
 interface GenshinImpactOSTAlbumWithDiscs extends GenshinImpactOSTAlbumBase {
-    discs: GenshinImpactOSTDisc[];
+  discs: GenshinImpactOSTDisc[];
 }
 
-type GenshinImpactOSTAlbum = GenshinImpactOSTAlbumWithDiscs | GenshinImpactOSTAlbumWithoutDiscs;
+type GenshinImpactOSTAlbum =
+  | GenshinImpactOSTAlbumWithDiscs
+  | GenshinImpactOSTAlbumWithoutDiscs;
 
 interface GenshinImpactOSTDisc {
-    name: string;
-    nameEn: string;
-    songs: GenshinImpactOSTSong[];
+  name: string;
+  nameEn: string;
+  songs: GenshinImpactOSTSong[];
 }
 
 interface GenshinImpactOSTSong {
-    name: string;
-    nameEn: string;
+  index: number;
+  name: string;
+  nameEn: string;
+  usages?: GenshinImpactOSTSongUsage[];
 }
+
+type GenshinImpactOSTSongUsage =
+  | GenshinImpactOSTSongUsageWorld
+  | GenshinImpactOSTSongUsageWorldBattle
+  | GenshinImpactOSTSongUsageBossBattle
+  | GenshinImpactOSTSongUsageDialog
+  | GenshinImpactOSTSongUsageUI
+  | GenshinImpactOSTSongUsageVideo;
+interface GenshinImpactOSTSongUsageWorld {
+  type: "WORLD";
+  area: string;
+  time?: "DAYTIME" | "NIGHTTIME";
+  timeDuration?: string;
+  weather?: "RAIN";
+}
+interface GenshinImpactOSTSongUsageWorldBattle {
+  type: "WORLD_BATTLE";
+  area: string;
+}
+interface GenshinImpactOSTSongUsageBossBattle {
+  type: "BOSS_BATTLE";
+  domain: string;
+  boss: string;
+}
+interface GenshinImpactOSTSongUsageDialog {
+  type: "DIALOG";
+  quest?: GenshinImpactOSTSongUsageQuest
+  description?: string;
+}
+interface GenshinImpactOSTSongUsageUI {
+  type: "UI";
+  description?: string;
+}
+interface GenshinImpactOSTSongUsageVideo {
+  type: "VIDEO";
+  videoType: "CG" | "PV";
+  quest?: GenshinImpactOSTSongUsageQuest;
+  description?: string;
+  video?: {
+    name: string;
+    bilibiliBv: string;
+  }
+}
+type GenshinImpactOSTSongUsageQuest = string;
