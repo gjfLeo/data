275c64eb04b88409c33d4a96bdfa68eec08ac2ae
Liger <gjfleo@yeah.net>
2024-03-04 23:43:08 +0800

    update

--- genshin-impact/locations.d.ts
+++ genshin-impact/locations.d.ts
@@ -1,9 +1,36 @@
-export type Data = Location[];
+export type Data = Region[];
 
-interface Location {
-  index?: number;
+interface LocationBase {
   name?: string;
   nameEn?: string;
+  level: number;
+}
+
+interface Region extends LocationBase {
+  name: string;
+  nameEn: string;
+  level: 1;
+  children?: Subregion[];
+}
+
+interface Subregion extends LocationBase {
+  index: number;
+  name?: string;
+  nameEn?: string;
+  level: 2;
+  children?: Area[];
+}
+
+interface Area extends LocationBase {
+  name: string;
+  nameEn: string;
+  level: 3;
+  children?: Location[];
+}
+
+interface Location extends LocationBase {
+  name: string;
+  nameEn: string;
   level: number;
   children?: Location[];
 }
